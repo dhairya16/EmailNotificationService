5e75974791f9581bb89fc54f9424127550db60ee
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-04-30 11:21:53 +0530

    Retryable exception simulation

--- src/main/java/com/dcode/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/dcode/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -2,19 +2,51 @@ package com.dcode.ws.emailnotification.handler;
 
 import com.dcode.ws.core.ProductCreatedEvent;
 import com.dcode.ws.emailnotification.error.NotRetryableException;
+import com.dcode.ws.emailnotification.error.RetryableException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @KafkaListener(topics = "product-created-events-topic", groupId = "product-created-events")
 public class ProductCreatedEventHandler {
+    private RestTemplate restTemplate;
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    public ProductCreatedEventHandler(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @KafkaHandler
     public void handle(ProductCreatedEvent productCreatedEvent) {
         LOGGER.info("Received a new event " + productCreatedEvent.getTitle());
+
+        String requestUrl = "http://localhost:8082/response/200";
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
+
+            if (response.getStatusCode().value() == HttpStatus.OK.value()) {
+                LOGGER.info("Received response from a remote service: " + response.getBody());
+            }
+        } catch (ResourceAccessException ex) {
+            LOGGER.error(ex.getMessage());
+            throw new RetryableException(ex);
+        } catch (HttpServerErrorException ex) {
+            LOGGER.error(ex.getMessage());
+            throw new NotRetryableException(ex);
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
+            throw new NotRetryableException(ex);
+        }
+
+
     }
 }
