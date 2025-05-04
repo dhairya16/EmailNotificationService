27d2c565ba7257fe6ace2820025a383f612963f4
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-05-04 19:45:31 +0530

    storing messageId in db

--- src/main/java/com/dcode/ws/emailnotification/entity/ProcessedEventEntity.java
+++ src/main/java/com/dcode/ws/emailnotification/entity/ProcessedEventEntity.java
@@ -1,10 +1,7 @@
 package com.dcode.ws.emailnotification.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -14,6 +11,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class ProcessedEventEntity implements Serializable {
     @Id
     @GeneratedValue
--- src/main/java/com/dcode/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/com/dcode/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -1,10 +1,14 @@
 package com.dcode.ws.emailnotification.handler;
 
 import com.dcode.ws.core.ProductCreatedEvent;
+import com.dcode.ws.emailnotification.entity.ProcessedEventEntity;
 import com.dcode.ws.emailnotification.error.NotRetryableException;
 import com.dcode.ws.emailnotification.error.RetryableException;
+import com.dcode.ws.emailnotification.repository.ProcessedEventRepository;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,17 +26,26 @@ import org.springframework.web.client.RestTemplate;
 @KafkaListener(topics = "product-created-events-topic", groupId = "product-created-events")
 public class ProductCreatedEventHandler {
     private RestTemplate restTemplate;
+    private ProcessedEventRepository processedEventRepository;
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    public ProductCreatedEventHandler(RestTemplate restTemplate) {
+    public ProductCreatedEventHandler(RestTemplate restTemplate, ProcessedEventRepository processedEventRepository) {
         this.restTemplate = restTemplate;
+        this.processedEventRepository = processedEventRepository;
     }
 
+    @Transactional
     @KafkaHandler
     public void handle(@Payload ProductCreatedEvent productCreatedEvent,
                        @Header("messageId") String messageId,
                        @Header(KafkaHeaders.RECEIVED_KEY) String messageKey) {
         LOGGER.info("Received a new event " + productCreatedEvent.getTitle());
+
+        ProcessedEventEntity existingRecord = processedEventRepository.findByMessageId(messageId);
+        if (existingRecord != null) {
+            LOGGER.info("Found a duplicate message id: {}", existingRecord.getMessageId());
+            return;
+        }
 
         String requestUrl = "http://localhost:8082/response/200";
         try {
@@ -52,6 +65,17 @@ public class ProductCreatedEventHandler {
             throw new NotRetryableException(ex);
         }
 
+        // Save a unique message id in database table
+        try {
+            processedEventRepository.save(
+                    ProcessedEventEntity.builder()
+                            .messageId(messageId)
+                            .productId(productCreatedEvent.getProductId())
+                            .build()
+            );
+        } catch (DataIntegrityViolationException ex) {
+            throw new NotRetryableException(ex);
+        }
 
     }
 }
